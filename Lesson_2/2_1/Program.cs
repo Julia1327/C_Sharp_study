9dee8d4d1e5f2d1510f62e8bf47daf68ae254ef5
«Julia» <juliya454@yandex.ru>
2023-06-29 16:25:36 +0300

    семинар 2 , 1 задача усложнила

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -34,12 +34,20 @@ int Max(int num)
     if (num / 10 > num % 10)
         return num / 10;
 
-    return num % 10;
 
+    else if (num / 10 == num % 10)
+    {
+        Console.WriteLine("Цифры равны");
+        return num / 10;
+    }
+
+    return num % 10;
 }
+
 int RNum = new Random().Next(10, 100);
 Console.WriteLine($"Было сгенерировано случайное двузначное число: {RNum}");
+// int RNum = 77;
+
 int result = Max(RNum);
 Console.WriteLine($"Наибольшая цифра числа: {result} ");
-
 
