1b45a0171e0698a5e46d755de0f5823a3b635b3f
«Julia» <juliya454@yandex.ru>
2023-07-06 19:05:21 +0300

    3 семинар, 1 задача

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+void Quarters(int num1)
+{
+    if (num1 == 1)
+        Console.WriteLine("x = от 0 до + бесконечности, y = от 0 до + бесконечности");
+    else if (num1 == 2)
+        Console.WriteLine("x = от - бесконечности до 0, y = от 0 до + бесконечности");
+    else if (num1 == 3)
+        Console.WriteLine("x = от - бесконечности до 0, y = от - бесконечности до 0");
+    else if (num1 == 4)
+        Console.WriteLine("x = от 0 до + бесконечности, y = от - бесконечности до 0");
+    else
+        Console.WriteLine("Введите номер четверти от 1 до 4 включительно");
+
+
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Quarters(num);
