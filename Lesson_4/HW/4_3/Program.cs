0a0e0e590dfda4fe46194f4f51261f6c78d5ccfa
«Julia» <juliya454@yandex.ru>
2023-07-10 20:36:31 +0300

    4 семинар

--- Lesson_4/HW/4_3/Program.cs
+++ Lesson_4/HW/4_3/Program.cs
@@ -4,4 +4,25 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(0, 2);
+        i++;
+    }
+}
+void printArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position] + " ");
+        position++;
 
+    }
+}
+int[] array = new int[8];
+FillArray(array);
+printArray(array);
