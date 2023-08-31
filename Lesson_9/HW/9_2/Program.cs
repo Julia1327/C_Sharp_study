b2a732ed4bfc4c0e29bd73b775923a59aa30ff76
«Julia» <juliya454@yandex.ru>
2023-08-31 22:45:40 +0300

    9 семинар, дз, 2

---
+++ Lesson_9/HW/9_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+
+static int Rec(int num, int end, int sum) {
+	if (num > end) {
+		return sum;
+	}
+	sum += num;
+	num += 1;
+	sum = Rec(num, end, sum);
+	return sum;
+}
+
+Console.WriteLine("Enter N & M");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Result:");
+int sum = Rec(N, M, 0);
+Console.WriteLine(sum);
